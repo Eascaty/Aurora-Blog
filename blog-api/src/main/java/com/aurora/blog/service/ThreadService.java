010dbc674d4237a8463b749e0f3788c9ec15f201
Eascaty <510252916@qq.com>
2022-01-26 16:53:49 +0800

    阅读次数bug 修改，封装下次数

--- blog-api/src/main/java/com/aurora/blog/service/ThreadService.java
+++ blog-api/src/main/java/com/aurora/blog/service/ThreadService.java
@@ -15,6 +15,7 @@ public class ThreadService {
     @Async("taskExecutor")
     public void updateArticleViewCount(ArticleMapper articleMapper, Article article) {
 
+
         int viewCounts = article.getViewCounts();
         Article articleUpdate = new Article();
         articleUpdate.setViewCounts(viewCounts + 1);
@@ -24,11 +25,14 @@ public class ThreadService {
         updateWrapper.eq(Article::getViewCounts,viewCounts);
 //         update article set view_count=100 where view_Count=99 and id=11
         articleMapper.update(articleUpdate, updateWrapper);
-        try {
-            Thread.sleep(5000);
-            System.out.println("更新完成了...");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(5000);
+//            System.out.println("更新完成了...");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+
+
     }
 }
