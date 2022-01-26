1e3ea7869da802aaa69b36ff56090d36a61228b7
Eascaty <510252916@qq.com>
2022-01-26 16:14:20 +0800

    线程池 乐观锁 更新阅读次数

--- blog-api/src/main/java/com/aurora/blog/service/ThreadService.java
+++ blog-api/src/main/java/com/aurora/blog/service/ThreadService.java
@@ -2,6 +2,8 @@ package com.aurora.blog.service;
 
 import com.aurora.blog.dao.mapper.ArticleMapper;
 import com.aurora.blog.dao.pojo.Article;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,18 @@ public class ThreadService {
 
 
     //    期望此操作在線程池 執行 不會影響原有的主線程
+    @Async("taskExecutor")
     public void updateArticleViewCount(ArticleMapper articleMapper, Article article) {
+
+        int viewCounts = article.getViewCounts();
+        Article articleUpdate = new Article();
+        articleUpdate.setViewCounts(viewCounts + 1);
+        LambdaUpdateWrapper<Article> updateWrapper =  new LambdaUpdateWrapper<>();
+        updateWrapper.eq(Article::getId,article.getId());
+//       设置一个 为了在多线程的环境下 线程安全
+        updateWrapper.eq(Article::getViewCounts,viewCounts);
+//         update article set view_count=100 where view_Count=99 and id=11
+        articleMapper.update(articleUpdate, updateWrapper);
         try {
             Thread.sleep(5000);
             System.out.println("更新完成了...");
