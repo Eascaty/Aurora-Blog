ec5686987d7db848ed05a0632bf7e3973f227557
Eascaty <510252916@qq.com>
2022-03-04 10:29:03 +0800

    docker 管理员端打包

--- blog-api/src/main/java/com/aurora/blog/Controller/CategoryController.java
+++ blog-api/src/main/java/com/aurora/blog/Controller/CategoryController.java
@@ -9,14 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("categorys")
+@RequestMapping({"categorys"})
 public class CategoryController {
-
     @Autowired
     private CategoryService categoryService;
 
+    public CategoryController() {
+    }
+
     @GetMapping
-    public Result listCategory() {
-        return categoryService.findAll();
+    public Result categories() {
+        return this.categoryService.findAll();
+    }
+
+    @GetMapping({"detail"})
+    public Result categoriesDetail() {
+        return this.categoryService.findAllDetail();
+    }
+
+    @GetMapping({"detail/{id}"})
+    public Result categoryDetailById(@PathVariable("id") Long id) {
+        return this.categoryService.categoryDetailById(id);
     }
 }
--- blog-api/src/main/java/com/aurora/blog/config/WebMvcConfig.java
+++ blog-api/src/main/java/com/aurora/blog/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
 //            跨域配置
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedOrigins("http://zhaoxiaolong.top");
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedOrigins("http://47.100.10.241:8080").allowedOrigins("http://zhaoxiaolong.top");
     }
 
     @Override
